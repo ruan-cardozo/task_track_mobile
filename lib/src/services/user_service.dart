68cde0519e25c4e79d75e649a9eea65bc32b2df7
ruan-cardozo <ruanvcardozo@gmail.com>
2024-05-14 23:34:44 -0300

    feat: Add error handling to register function in UserService

--- lib/src/services/user_service.dart
+++ lib/src/services/user_service.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
-
 import 'package:http/http.dart' as http;
 
 class UserService {
@@ -11,8 +9,9 @@ class UserService {
   Future<http.Response> register(String name, String email, String password) async {
 
     var urlToPost = '$baseUrl/v2/users';
-    
-    var result = await http.post(
+
+    try {
+      var result = await http.post(
       Uri.parse(urlToPost),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
@@ -22,14 +21,21 @@ class UserService {
       }),
     );
 
-    return result;
+     return result;
+    } catch (e) {
+
+      print('An error occurred: $e');
+
+      return http.Response('An error occurred: $e', 500);
+    }
+
+
+   
   }
 
   Future<http.Response> login(String email, String password) async {
-    print(email + password);
 
     var urlToPost = '$baseUrl/v2/login';
-    print(urlToPost);
 
     try {
       var result = await http.post(
@@ -38,11 +44,11 @@ class UserService {
         body: jsonEncode({'email': email, 'password': password}),
       );
 
-      print({result});
-
       return result;
     } catch (e) {
+
       print('An error occurred: $e');
+
       return http.Response('An error occurred: $e', 500);
     }
   }
